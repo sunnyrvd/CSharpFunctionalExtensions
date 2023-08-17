feb6445166e6981be67e1d2a42352d47c46edcea
Abdullah <abdullah.gb@hotmail.com>
2023-08-17 02:54:23 -0700

    fluent assertion namespace fix

---
+++ FluentAssertions.CSharpFunctionalExtensions/ResultAssertions.cs
@@ -0,0 +1,18 @@
+﻿using Metaphor.Csharp.Extensions;
+using FluentAssertions.Execution;
+using System;
+
+namespace FluentAssertions.CSharpFunctionalExtensions
+{
+    public class ResultAssertions : BaseResultAssertions<Result>
+    {
+        public ResultAssertions(Result subject) : base(subject)
+        {
+        }
+
+        protected override Func<FailReason> GetErrorMessageFromSubject()
+        {
+            return () => new FailReason($"Expected {{context:result}} to be Success{{reason}}, but it is failure with error '{Subject.Error}'");
+        }
+    }
+}
---
+++ FluentAssertions.CSharpFunctionalExtensions/ResultTAssertions.cs
@@ -0,0 +1,18 @@
+﻿using Metaphor.Csharp.Extensions;
+using FluentAssertions.Execution;
+using System;
+
+namespace FluentAssertions.CSharpFunctionalExtensions
+{
+    public class ResultTAssertions<T> : BaseResultAssertions<Result<T>>
+    {
+        public ResultTAssertions(Result<T> subject) : base(subject)
+        {
+        }
+
+        protected override Func<FailReason> GetErrorMessageFromSubject()
+        {
+            return () => new FailReason($"Expected {{context:result}} to be Success{{reason}}, but it is failure with error '{Subject.Error}'");
+        }
+    }
+}
